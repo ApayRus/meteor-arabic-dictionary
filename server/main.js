9d0165b6eeeebdf83a9060d1078c3bec4a63d00a
Aparus <rustamapaev@gmail.com>
2017-08-18 12:43:46 +0300

    s3 hosting for images

--- server/main.js
+++ server/main.js
@@ -3,6 +3,7 @@ import { Meteor } from 'meteor/meteor';
 import { Articles } from '../imports/api/articles';
 import { Events } from '../imports/api/events';
 import { Images } from '../imports/api/images';
+import '/imports/server/server-side-file-store.js';
 Meteor.startup(() => {
   // code to run on server at startup
 });
