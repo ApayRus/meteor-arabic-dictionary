c3a626d744b32bc1499e6a43be93c3bdf2b3b9fa
Aparus <rustamapaev@gmail.com>
2017-10-12 01:02:34 +0300

    edit form

--- client/ui/components/ArticleForm.js
+++ client/ui/components/ArticleForm.js
@@ -7,7 +7,7 @@ Template.ArticleForm.onCreated(function(){
         self.subscribe("articleSingle", self.data._id);
     }); */
     this.reactiveForm = new ReactiveDict();
-    this.reactiveForm.set('article', this.data)
+    //this.reactiveForm.set('article', this.data)
     /*
     this.reactiveForm.set(this)
     console.log('reactiveDict', this.reactiveForm)
@@ -16,10 +16,32 @@ Template.ArticleForm.onCreated(function(){
 
 Template.ArticleForm.helpers({
     article(){
+        console.log('this', this)
+        let newWords = this.words.map((elem, index) => {
+            return {note: elem.note, word: elem.word, wordId: `article.${this._id}.words.${index}`}
+        })
+
+        let newTranslations = this.translations.map((elem, index) => {
+            return {
+                    translation: elem.translation, 
+                    translationId: `article.${this._id}.translations.${index}.translation`, 
+                    examples: elem.examples.map((elem2, index2)=>{
+                        return {
+                                example: elem2.example,
+                                translation: elem2.translation,
+                                exampleId: `article.${this._id}.translations.${index}.examples.${index2}`
+                            }
+                    })
+                }
+        })
+
+        //this.words = newWords
+        //this.translations = newTranslations
+        Template.instance().reactiveForm.set("article", this)
         const article = Template.instance().reactiveForm.get("article"); 
         //console.log('this.reactiveForm', )
         return article
-    }
+    },
 });
 
 //Template.ArticleMenu.inheritsHelpersFrom('ArticlePage');
